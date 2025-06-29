c87086db9d1fceee195ba27ca349dcc3828e3b85
Natalia Arevalo <n.arevalo89@hotmail.com.ar>
2025-06-28 21:07:01 -0300

    readme

--- src/app/futbolfemhaedo/page.jsx
+++ src/app/futbolfemhaedo/page.jsx
@@ -12,40 +12,4 @@ const futbolFemHaedo = () => {
 export default futbolFemHaedo
 
 
-    // <div className='flex flex-col gap-4 items-center p-2'>
-    //       <div className='relative z-10'>
-    //         <Image src="/Escudo_Haedo_transparencia.png" alt="escudo Haedo" width={150} height={150}
-    //         style={{filter: "drop-shadow(-10px 4px 10px  rgb(203, 49, 49)"}} />
-    //       </div>
-    //       <div>
-    //         <h1 className='text-red-400 text-3xl '>{club.club}</h1>           
-    //         <p className='text-white '>Estamos en: {club.ubicacion.nombre} <br />
-    //         <span >
-    //           <ArrowOutwardIcon className='text-red-400 '/>
-    //         </span> 
-    //         {club.ubicacion.direccion}</p>
-    //         <h2 className='text-white '> 
-    //           <span>
-    //           <CalendarMonthIcon className='text-red-500  '/>
-    //           </span>
-    //         Días y Horarios:         
-    //         </h2>
-    //         <ul>
-    //           {club.dias_y_horarios.map((dia, index) => (
-    //             <li key={index} className='text-white'>{dia.dia}: {dia.horario}</li>
-    //           ))}
-    //         </ul>
-    //       </div>
-    //       <div className='text-white mb-4'>
-    //         <h2>Categorias</h2>
-    //         <ul>
-    //           {club.categorias.map((categoria, index) => (
-    //             <li key={index} className='text-white'>✔️{categoria.nombre}: {categoria.edades}</li>
-    //           ))}
-    //         </ul>
-            
-    //       </div>
-    //     </div>
- 
-
-{/* <h2 className='text-red-200'>✔️ +17- Adultas</h2> */}
+    
--- src/app/futbolfempalomar/page.jsx
+++ src/app/futbolfempalomar/page.jsx
@@ -9,5 +9,3 @@ const futbolFemPalomar = () => {
 
 export default futbolFemPalomar
 
-// rgb(247, 141, 42)
-//style={{filter: "drop-shadow(-10px 4px 10px  rgb(217, 159, 104)"}} 
