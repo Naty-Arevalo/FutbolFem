227ca92a6f154b1877e9d520018c62b7edcdd230
Natalia Arevalo <n.arevalo89@hotmail.com.ar>
2025-08-15 19:22:12 -0300

    Agrego sitemap y robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://futbol-fem.vercel.app/', // Cambia por tu dominio real
+    generateRobotsTxt: true,
+    outDir: './public', // Muy importante para que aparezca en producción
+  };
+  
